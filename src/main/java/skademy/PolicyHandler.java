4fbbd4b5d38329d94a7f83d53a02ce72c3ca2c3d
JJongMani <3827925@naver.com>
2020-04-21 16:04:22 +0900

    commit

--- src/main/java/skademy/PolicyHandler.java
+++ src/main/java/skademy/PolicyHandler.java
@@ -12,22 +12,14 @@ import java.util.Optional;
 
 @Service
 public class PolicyHandler{
-    @Autowired
-    CourseRegistrationSystemRepository courseRegistrationSystemRepository;
-
     @StreamListener(KafkaProcessor.INPUT)
     public void wheneverCourseCanceled_결제취소(@Payload CourseCanceled courseCanceled){
         if(courseCanceled.isMe()){
 //            System.out.println("##### listener 결제취소 : " + courseCanceled.toJson());
             System.out.println("강의 " + courseCanceled.getId() + "번 취소되었습니다.");
 
-            Optional<CourseRegistrationSystem> courseRegistrationSystemOptional = courseRegistrationSystemRepository.findById(courseCanceled.getId());
-            CourseRegistrationSystem courseRegistrationSystem = courseRegistrationSystemOptional.get();
-            System.out.println("2 ##" + courseRegistrationSystem + "##");
-            courseRegistrationSystem.setStatus("취소 완료");
 
-            courseRegistrationSystemRepository.save(courseRegistrationSystem);
-        }
+    }
     }
 
 }
